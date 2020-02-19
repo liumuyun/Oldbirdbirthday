5dd21836b70902c0adfe937c9e49ef8c78892b3b
liumuyun <57094723+liumuyun@users.noreply.github.com>
2020-02-19 23:57:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      */
     imgs: {
         "祝你永远幸福":  "./imgs/liu.jpg",
-        "生日快乐":  "./imgs/zf.jpg",
+        "生日快乐":  "./imgs/zf.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
